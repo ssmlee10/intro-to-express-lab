ff22a0ce2b3ed16fa2ae2847fb7593579c9d11f5
Sharon Lee <ssmlee10@gmail.com>
2025-07-15 13:09:20 -0700

    complete exercise 1

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require("express");
 
 const app = express();
+
+// Exercise 1
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Hello there, ${req.params.username}!`);
+});
+
+
+app.listen(3000, () => {
+    console.log('Listening on Port 3000');
+});

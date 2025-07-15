0be59cae0b541c784efeeab5ad27ddaf67a07ce8
Sharon Lee <ssmlee10@gmail.com>
2025-07-15 13:39:15 -0700

    complete exercise 2

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/greetings/:username', (req, res) => {
     res.send(`Hello there, ${req.params.username}!`);
 });
 
+// Exercise 2
+app.get('/roll/:number', (req, res) =>{
+
+    if (!Number(req.params.number)) {
+        return res.send(`You must specify a number!`);
+    }
+    const roll = Math.floor(Math.random() * (req.params.number) + 1);
+    return res.send(`You rolled a ${roll}`);
+});
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
